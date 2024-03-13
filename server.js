881f984d2409d8c0fdc03d557491a987a8e394ec
MatheusDiniz <89808175+Madtheusss@users.noreply.github.com>
2024-03-13 16:03:43 -0300

    finish

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app.js');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log('Server is Alive!');
+  console.log('servidor escutando!');
 });
--- src/services/Services.js
+++ src/services/Services.js
@@ -1,12 +1,34 @@
-const dataSource = require('../models')
+const dataSource = require('../models');
 
 class Services {
-  constructor(modelName){
-    this.model = modelName;
+  constructor(nomeDoModel) {
+    this.model = nomeDoModel;
   }
 
-  async getAllRegister(){
+  async pegaTodosOsRegistros () {
     return dataSource[this.model].findAll();
+  }
+
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id }
+    });
+    if (listadeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  async excluiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
   }
 }
 
